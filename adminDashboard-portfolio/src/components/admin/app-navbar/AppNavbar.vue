c02293c148bdb5be4fecb391c87bad61d6d6c97c
maydany <ishlove89@naver.com>
2018-08-19 13:21:36 +0900

    New: Start new admin-dashboard project

---
+++ adminDashboard-portfolio/src/components/admin/app-navbar/AppNavbar.vue
@@ -0,0 +1,54 @@
+<template>
+
+  <vuestic-navbar>
+    <header-selector slot="selector" :isOpen.sync="valueProxy"/>
+    <span slot="center" class="h2 pt-2 mt-4">
+      {{$t('navbar.messageUs')}}
+    </span>
+    <message-dropdown/>
+    <notification-dropdown/>
+    <language-dropdown/>
+    <profile-dropdown>
+      <img src="https://avatars3.githubusercontent.com/u/38318319?s=460&v=4"/>
+    </profile-dropdown>
+  </vuestic-navbar>
+
+</template>
+
+<script>
+  import VuesticNavbar from '../../../vuestic-theme/vuestic-components/vuestic-navbar/VuesticNavbar'
+  import HeaderSelector from './components/HeaderSelector'
+  import LanguageDropdown from './components/dropdowns/LanguageDropdown'
+  import ProfileDropdown from './components/dropdowns/ProfileDropdown'
+  import NotificationDropdown from './components/dropdowns/NotificationDropdown'
+  import MessageDropdown from './components/dropdowns/MessageDropdown'
+
+  export default {
+    name: 'app-navbar',
+
+    components: {
+      VuesticNavbar,
+      HeaderSelector,
+      MessageDropdown,
+      NotificationDropdown,
+      LanguageDropdown,
+      ProfileDropdown
+    },
+    props: {
+      isOpen: {
+        type: Boolean,
+        required: true
+      }
+    },
+    computed: {
+      valueProxy: {
+        get () {
+          return this.isOpen
+        },
+        set (opened) {
+          this.$emit('toggle-menu', opened)
+        },
+      },
+    },
+  }
+</script>
