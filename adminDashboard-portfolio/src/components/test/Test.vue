e2f694e01fde042e08e6bec75c1aedf0f26bbf2e
maydany <ishlove89@naver.com>
2018-08-20 22:59:48 +0900

    Feat: Create page and data for Test menu

---
+++ adminDashboard-portfolio/src/components/test/Test.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="charts-page">
+      <div class="col-md-6">
+        <vuestic-widget class="chart-widget" :headerText="'charts.horizontalBarChart' | translate">
+          <vuestic-chart :data="TestHorizontalBarChartData" type="horizontal-bar"></vuestic-chart>
+        </vuestic-widget>
+      </div>
+      <div>{{ TestHorizontalBarChartData.datasets[0].data[0] }}</div>
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import TestHorizontalBarChartData from 'data/charts/TestHorizontalBarChartData'
+  import SidebarLink from 'components/admin/app-sidebar/components/SidebarLink'
+
+  export default {
+    name: 'charts',
+    components: {
+      SidebarLink
+    },
+
+    data: function () {
+      return {
+        TestHorizontalBarChartData: TestHorizontalBarChartData
+      }
+    },
+  }
+</script>
+
+<style lang="scss">
+  .widget.chart-widget {
+    .widget-body {
+      height: 500px;
+    }
+  }
+</style>
